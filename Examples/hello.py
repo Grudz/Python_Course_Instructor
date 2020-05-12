c5ad75119d526522b3b5bf937de3030c685d8916
Ben Grudzien <bgrudzien@oakland.edu>
2020-05-12 15:02:00 -0400

    Create hello.py

---
+++ Examples/hello.py
@@ -0,0 +1,10 @@
+# First Python Program!
+
+print("What is your name?")
+myName = input() # Taking user input
+print("Your name, " + myName)
+print(len(myName)) # Getting the length of myName 
+
+print("What is your age?")
+myAge = input()
+print("In one year, you will be " + str(int(myAge) + 1) + ", congrats") # Convert input str to int
