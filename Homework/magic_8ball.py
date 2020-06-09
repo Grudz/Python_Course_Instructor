8a82d7ed3e9c1f1bfb7c5d6b9b8d66f1191f860e
Ben Grudzien <bgrudzien@oakland.edu>
2020-06-09 18:24:34 -0400

    Add files via upload

---
+++ Homework/magic_8ball.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun  7 11:54:36 2020
+@author: alyxandrahoberg
+"""
+
+
+# MAGIC 8-BALL HOMEWORK
+
+import random
+
+history = ["History:"]
+
+def magiceightball(random_number):
+    
+   if random_number == 1:
+       result = "Yes!"
+
+   if random_number == 2:
+       result = "No."
+
+   if random_number == 3:
+       result = "Maybe."
+
+   if random_number == 4:
+       result = "Possibly."
+
+   if random_number == 5: 
+       result = "You may rely on it."
+
+   if random_number == 6: 
+       result = "Try Again."
+
+   history.append(result)
+   print(result)
+    
+ 
+
+# 8 ball 
+print("---- Welcome to the Magic 8 Ball! -----\n")
+
+while True: 
+    print("What is your Question?\n")
+    question = input()
+    history.append(question)
+    
+    answer = random.randint(1,6)
+    magiceightball(answer)
+
+    print("Press H to see History, Press E to Exit, Press Enter to Ask Again\n")
+    decision = input()
+    if decision == "E":
+        break
+    if decision == "H":
+        print(history)
+        input("Press ENTER to Ask Again")
+
+        
